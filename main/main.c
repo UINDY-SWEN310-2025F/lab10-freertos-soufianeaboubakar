623dc6f875accbea35f4a6ac31dcb3a123a1beb4
Soufiane Aboubakar <soufianeaboubakar@Soufianes-MBP.lan>
2025-11-16 00:04:05 -0500

    Lab 10

--- main/main.c
+++ main/main.c
@@ -3,6 +3,7 @@
 #include "freertos/task.h"
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 // ---------------------- TASK 1 ------------------------
 void task1(void *p) {
     int count = 0;
@@ -50,31 +51,39 @@ void app_main(void) {
 // --- Task Functions ----------------------------------------------------
 
 void taskFirst(void *p) {
+=======
+void task1(void *p) {
+>>>>>>> 4387a2b (Lab 10)
     int count = 0;
-    while (1) {
+    for (int i = 0; i < 3; i++) {
         printf("hello task1: count %d\n", count++);
         vTaskDelay(500 / portTICK_PERIOD_MS);
     }
+    vTaskDelete(NULL);
 }
 
-void taskSecond(void *p) {
+void task2(void *p) {
     int count = 0;
-    while (1) {
+    for (int i = 0; i < 3; i++) {
         printf("hello task2: count %d\n", count++);
         vTaskDelay(500 / portTICK_PERIOD_MS);
     }
+    vTaskDelete(NULL);
 }
 
-void taskThird(void *p) {
+void task3(void *p) {
     int count = 0;
-    while (1) {
+    for (int i = 0; i < 3; i++) {
         printf("hello task3: count %d\n", count++);
         vTaskDelay(500 / portTICK_PERIOD_MS);
     }
+    vTaskDelete(NULL);
 }
 
-// --- app_main ----------------------------------------------------------
+void app_main(void) {
+    printf("Starting FreeRTOS...\n");
 
+<<<<<<< HEAD
 void app_main(void)
 {
     // Create three tasks with different priorities
@@ -82,5 +91,16 @@ void app_main(void)
     xTaskCreate(taskSecond, "task2", 2048, NULL, 2, NULL);   // Medium priority
     xTaskCreate(taskThird,  "task3", 2048, NULL, 1, NULL);   // Lowest priority
 >>>>>>> 8c9574a (Lab11 Real-Time OS)
+=======
+    // Must include 3 xTaskCreate() for autograder
+    xTaskCreate(task1, "t1", 2048, NULL, 1, NULL);
+    xTaskCreate(task2, "t2", 2048, NULL, 1, NULL);
+    xTaskCreate(task3, "t3", 2048, NULL, 1, NULL);
+
+    // Optional: delay before restart message
+    vTaskDelay(5000 / portTICK_PERIOD_MS);
+
+    printf("Restarting after 5 seconds...\n");
+>>>>>>> 4387a2b (Lab 10)
 }
 
